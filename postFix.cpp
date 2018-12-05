a4ecceea66837d0e215f2376e4426e39ebe4b9a4
Mi'Angel Pata <39149050+Miapata@users.noreply.github.com>
2018-12-05 12:20:38 -0700

    Update postFix.cpp

--- postFix.cpp
+++ postFix.cpp
@@ -1,4 +1,7 @@
-#include "pch.h"
+// POSTFIX.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
 
 // C++ program to evaluate value of a postfix expression  
 #include <iostream>  
@@ -13,19 +16,41 @@ stack<int> ourStack;
 // The main function that returns value of a given postfix expression  
 int evaluatePostfix(string exp)
 {
-	// Create a stack of capacity equal to expression size  
-
-
-
-	// See if stack was created successfully  
-
 
 	// Scan all characters one by one  
-	string  multiDigit = "";
 	for (int i = 0; i < exp.length(); ++i)
 	{
+		//We check if the character is a digit
 		if (isdigit(exp[i])) {
-			ourStack.push(exp[i]);
+			
+			//If it is a digit, check the next character to see if it is a digit
+			if (isdigit(exp[i + 1])) {
+				string digit;
+				digit += exp[i];
+				//place
+				int index = i + 1;
+
+				//While the next index is a digit, add it to the digit string
+				//When it is done, push the integer to the stack
+				while (isdigit(exp[index]))
+				{
+					digit += exp[index];
+					index++;
+				}
+				i = index;
+				int number = stoi(digit);
+				ourStack.push(number);
+
+			}
+
+			//If the next index is not a digit, push our current index to the stack
+			else
+			{
+				ourStack.push(exp[i] - '0');
+			}
+		}
+		//If it is a space, do nothing
+		else if (isspace(exp[i])) {
 
 		}
 
@@ -34,21 +59,26 @@ int evaluatePostfix(string exp)
 		// elements from stack apply the operator  
 		else
 		{
+
 			int val1 = ourStack.top();
 			ourStack.pop();
 
 			int val2 = ourStack.top();
 			ourStack.pop();
 
+			//Here we are able to do the operations
 			switch (exp[i])
 			{
-			case '+': ourStack.push(val2 + val1); break;
-			case '-': ourStack.push(val2 - val1); break;
-			case '*': ourStack.push(val2 * val1); break;
-			case '/': ourStack.push(val2 / val1); break;
+			case '+': ourStack.push(val2 + val1); cout << "Used values: " << val2 << " & " << val1 << endl; break;
+			case '-': ourStack.push(val2 - val1); cout << "Used values: " << val2 << " & " << val1 << endl; break;
+			case '*': ourStack.push(val2 * val1); cout << "Used values: " << val2 << " & " << val1 << endl; break;
+			case '/': ourStack.push(val2 / val1); cout << "Used values: " << val2 << " & " << val1 << endl; break;
+			case ' ': break;
+
 			default: break;
 			}
 		}
+
 	}
 
 	// If the scanned character is an operand (number here),  
@@ -58,13 +88,15 @@ int evaluatePostfix(string exp)
 	return ourStack.top();
 }
 
-// Driver program to test above functions  
+// Main program to test above functions  
 int main()
 {
 	char exprChar[1000];
 	cin.getline(exprChar, 1000);
 	string exp = exprChar;
 
-	cout << "postfix evaluation: " << evaluatePostfix(exp);
+	cout << "postfix evaluation: " << evaluatePostfix(exp) << endl;
+	system("pause");
 	return 0;
+
 }
